c1ce01f7a3f441776582794332e01f911646c7a1
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-10 03:46:29 +0000

    Create psql connection with rust

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use postgres::{Client, Error, NoTls};
 fn main() -> Result<(), Error> {
     let mut client = Client::connect(
-        "postgresql://dboperator:operatorpass123@localhost:5243/postgres",
+        "postgresql://nobody:password@localhost:5243/postgres",
         NoTls,
     )?;
     Ok(())
