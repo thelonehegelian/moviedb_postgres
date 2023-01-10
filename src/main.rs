9382de7a803fea16f39adaa8f78b8229d8444518
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-10 04:16:50 +0000

    Create insert query

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use postgres::{Client, Error, NoTls};
+use std::collections::HashMap;
 fn main() -> Result<(), Error> {
     // postgres link format: postgresql://[user[:password]@][netloc][:port][/dbname][?param1=value1&...]
     let mut client = Client::connect("postgresql://nobody:password@localhost:5243/library", NoTls)?;
@@ -16,6 +17,45 @@ fn main() -> Result<(), Error> {
         director_id INTEGER NOT NULL REFERENCES director
     )",
     )?;
+    // INSERT AND FETCH DATA
+
+    struct Director {
+        id: i32,
+        name: String,
+    }
+    struct Movie {
+        title: String,
+        director_id: i32,
+    }
+
+    let mut movies = HashMap::new();
+    let mut directors = HashMap::new();
+    movies.insert(1, String::from("Jaws"));
+    movies.insert(2, String::from("The Matrix"));
+    directors.insert(1, String::from("Steven Spielberg"));
+    directors.insert(2, String::from("Wachowski Brothers"));
+
+    for (key, value) in directors {
+        let director = Director {
+            id: key,
+            name: value.to_string(),
+        };
+        client.execute(
+            "INSERT INTO director (id, name) VALUES ($1, $2)",
+            &[&director.id, &director.name],
+        )?;
+    }
+
+    for (key, value) in movies {
+        let movie = Movie {
+            title: value.to_string(),
+            director_id: key,
+        };
+        client.execute(
+            "INSERT INTO movie (title, director_id) ($1, $2)",
+            &[&movie.title, &movie.director_id],
+        )?;
+    }
 
     Ok(())
 }
